cc24a8e51c24c1eb2f757bf36ae7a05bf8813f9a
@Shoveet2003 <shoveetsingh2002@gmail.com>
2024-04-07 01:01:32 +0530

    mobile responsiveness added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -19,10 +19,10 @@ const Profile = () => {
   return (
       <div className="flex flex-col">
         <h1 className="font-serif text-pink-500 font-bold text-xl p-6">dribble</h1>
-        <h2 className="font-serif py-4 justify-center items-center px-96 text-2xl font-semibold">Welcome! Let's create your profile</h2>
-        <p className="font-sans px-96 text-gray-400 text-sm font-bold">Let others get to know you better!</p>
-        <p className="font-serif py-6 px-96 text-lg font-semibold">Add an Avatar</p>
-        <div className=" relative px-96">
+        <h2 className="font-serif py-4 justify-center items-center md:px-96 text-2xl font-semibold">Welcome! Let's create your profile</h2>
+        <p className="font-sans md:px-96 text-gray-400 text-sm font-bold">Let others get to know you better!</p>
+        <p className="font-serif py-6 md:px-96 text-lg font-semibold">Add an Avatar</p>
+        <div className=" relative md:px-96">
         {selectedImage ? (
             <div className="flex items-center mt-4 py-6">
               <img src={selectedImage} alt="Selected" className="w-32 h-32 object-cover rounded-full" />
@@ -37,14 +37,13 @@ const Profile = () => {
             Choose Image
         </label>
         </div>  
-        <div className = " flex-col px-96 py-8">
+        <div className = "sm:flex-row  md:px-96 py-8">
         <h1 className="font-bold text-lg font-serif">Add your location</h1>
         <input type="text" className="border-2 border-gray-300 rounded-md m-2 px-4 py-2"/>
         <div className="flex flex-row p-2">
         <button className="border-2 px-10 py-2  bg-pink-600 text-white  m-2 rounded-md font-semibold">Next</button>
         <h1 className="text-gray-400 font-serif text-sm py-4">or press return</h1>
-        </div>
-        
+        </div>        
         </div>
       </div>
   );
