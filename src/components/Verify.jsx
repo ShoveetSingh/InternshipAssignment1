31ee8388ec641f3a8947e3ab3dd7eb13fc527e27
@Shoveet2003 <shoveetsingh2002@gmail.com>
2024-04-09 01:05:51 +0530

    Verify pg completed!

--- src/components/Verify.jsx
+++ src/components/Verify.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react'
 import { FiSearch } from 'react-icons/fi'
 import { FaEnvelopeCircleCheck } from 'react-icons/fa6'
 import { FaInstagram,FaTwitter,FaFacebook,FaPinterest } from 'react-icons/fa6'
+import { FaDribbble } from 'react-icons/fa6'
 
 const Verify = () => {
   return (
@@ -41,20 +42,82 @@ const Verify = () => {
       <div className="bg-gray-100">
         <div className="flex flex-row">
         <div className="text-pink-400 font-semibold text-xl font-serif px-6 py-8">dribble</div>
-        <div className=" font-serif font-bold py-9 px-14 text-xs">For Designers</div>
-        <div className=" font-serif font-bold py-9 px-14 text-xs">Hire designers</div>
-        <div className=" font-serif font-bold py-9 px-14 text-xs">Company</div>
+        <div className=" font-serif font-bold py-9 px-16 text-xs">For Designers</div>
+        <div className=" font-serif font-bold py-9 px-12 text-xs">Hire designers</div>
+        <div className=" font-serif font-bold py-9 px-16 text-xs">Company</div>
         <div className=" font-serif font-bold py-9 px-14 text-xs">Directories</div>
-        <div className=" font-serif font-bold py-9 px-14 text-xs">Company Resources</div>
+        <div className=" font-serif font-bold py-9 px-16 text-xs">Company Resources</div>
         </div>
-       <div className="font-serif text-xs  py-2 px-6">
-        Dribble is the world<br/> leading community <br/>for creatives to share,<br/>grow,and get hired.
-        </div>
-        <div className="flex flex-row py-2 items-center px-6">
+       <div className="font-serif  text-xs  py-2 px-6 flex flex-row">
+       <div className="flex flex-col">
+       Dribble is the world<br/> leading community <br/>for creatives to share,<br/>grow,and get hired.
+        <div className="flex flex-row py-4 px-5"> 
          <FaInstagram className="text-pink-400 font-xs"/>
           <FaTwitter className="text-pink-400 font-xs"/>
           <FaFacebook className="text-pink-400 font-xs"/>
           <FaPinterest className="text-pink-400 font-xs"/>
+        </div>
+       </div>
+        <div className="px-12 flex flex-col">
+         <div>Go Pro!</div>
+        <div className="py-3">Explore design work</div>
+        <div className="py-1">Design blog</div>
+        <div className="py-2">Overtime Podcast</div>
+        <div className="py-2">Weekly warmup</div>
+        <div className="py-2">Refer a Friend</div>
+        <div className="py-2">Code of conduct</div>
+        </div>
+        <div className="px-10 flex flex-col">
+        <div>Post a job opening</div>
+        <div className="py-3">Post a freelance project</div>
+        <div className="py-1">Search for designers</div>
+        <div className="py-4 font-bold italic">Brands</div>
+        <div className="py-2">Advertise with us</div>
+        </div>
+        <div className="px-11 flex flex-col">
+         <div>About</div>
+        <div className="py-3">Careers</div>
+        <div className="py-1">Support</div>
+        <div className="py-2">Medical</div>
+        <div className="py-2">Testimonials</div>
+        <div className="py-2">API</div>
+        <div className="py-2">Terms of service</div>
+        <div className="py-2">Privacy policy</div>
+        <div className="py-2">Cookie policy</div>
+        </div>
+        <div className="px-12 flex flex-col">
+         <div>Design jobs</div>
+        <div className="py-3">Designers for hire</div>
+        <div className="py-1">freelance designers<br/> for hire</div>
+        <div className="py-2">Tags</div>
+        <div className="py-2">Places</div>
+        <div className="py-4 font-bold italic">Design assets</div>
+        <div className="py-2">Dribble Marketplace</div>
+        <div className="py-2">Creative market</div>
+        <div className="py-2">Fontspring</div>
+        <div className="py-2">Font Squirrel</div>
+        <div className="py-4"></div>
+        </div>
+        <div className="px-8 flex flex-col">
+         <div>Freelancing</div>
+        <div className="py-3">Design Hiring</div>
+        <div className="py-1">Design portfolio</div>
+        <div className="py-2">Design Education</div>
+        <div className="py-2">Creative Process</div>
+        <div className="py-2">Design Industry<br/> Trends</div>
+        </div>
+        <div>
+          
+          <hr className="w-full"/>
+        </div>
+        </div>
+        <div className="flex flex-row">
+        <div className="text-xs text-gray-500 font-bold font-ariel py-4 px-12">@ 2023 dribble all rights reserved</div>
+        <div className="px-80"></div>
+        <div className="flex flex-row text-gray-500 font-semibold font-ariel py-4  text-xs">
+          20,501,853 shots dribbbled  <div className="px-2 py-0"><FaDribbble className="text-pink-400 text-xs"/></div>
+        </div>
+          
         </div>
         </div>
     </div>
